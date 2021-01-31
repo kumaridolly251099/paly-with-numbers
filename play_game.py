2943f4b6918cddf1e86ead9ba6d7501a00f91667
kumaridolly251099 <kridoly251099@gmail.com>
2021-01-31 13:44:00 +0530

    first commit

---
+++ play_game.py
@@ -0,0 +1,21 @@
+import random
+import math
+
+n = random.randint(0,9)
+
+x = int(input("take the no what you want!: "))
+
+y = int(input("same no took from your frnd!: "))
+
+print(f"i am giving you a single random no {n}")
+
+b = int(input("the sum of total no you have!:"))
+
+z = int(input("divid your whole no into 2!: "))
+
+a = int(input("now subtract the no which you taken from you frnd!:"))
+
+print(f"now yours final no is :{a} ")
+
+
+
